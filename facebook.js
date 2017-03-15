82f9e89f002ef025af4068b9eec4620098897b18
Umisagi <zenchiter@gmail.com>
2017-03-15 14:05:04 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -24,9 +24,21 @@ const fbMessage = (recipientId, msg, cb) => {
       recipient: {
         id: recipientId,
       },
-      message: {
-        text: msg,
+     message:{
+      text:"Pick a color:",
+    quick_replies:[
+      {
+        content_type:"text",
+        title:"Red",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
       },
+      {
+        content_type:"text",
+        title:"Green",
+        payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
+  },
     },
   };
 
