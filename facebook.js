4d30b4b17777ababcc7b61e027300188156be9aa
Umisagi <zenchiter@gmail.com>
2017-03-16 10:50:17 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -24,8 +24,26 @@ const fbMessage = (recipientId, msg, cb) => {
         id: recipientId,
       },
       message: {
-        text: msg,
-      },
+    attachment:{
+      type:"template",
+      payload:{
+        template_type:"button",
+        text:"What do you want to do next?",
+        buttons:[
+          {
+            type:"web_url",
+            url:"https://petersapparel.parseapp.com",
+            title:"Show Website"
+          },
+          {
+            type:"postback",
+            title:"Start Chatting",
+            payload:"USER_DEFINED_PAYLOAD"
+          }
+        ]
+      }
+    }
+  },
     },
   };
 
