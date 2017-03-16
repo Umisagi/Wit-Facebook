56cbdb0d5b10e6cbc39a7c95d5918bc43934b608
Umisagi <zenchiter@gmail.com>
2017-03-16 11:07:59 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -23,22 +23,34 @@ const fbMessage = (recipientId, msg, cb) => {
       recipient: {
         id: recipientId,
       },
-      message: {
+      message:{
     attachment:{
       type:"template",
       payload:{
-        template_type:"button",
-        text:"What do you want to do next?",
-        buttons:[
-          {
-            type:"web_url",
-            url:"https://petersapparel.parseapp.com",
-            title:"Show Website"
-          },
-          {
-            type:"postback",
-            title:"Start Chatting",
-            payload:"USER_DEFINED_PAYLOAD"
+        template_type:"generic",
+        elements:[
+           {
+            title:"Welcome to Peter\'s Hats",
+            image_url:"https://petersfancybrownhats.com/company_image.png",
+            subtitle:"We\'ve got the right hat for everyone.",
+            default_action: {
+              type: "web_url",
+              url: "https://peterssendreceiveapp.ngrok.io/view?item=103",
+              messenger_extensions: true,
+              webview_height_ratio: "tall",
+              fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+            },
+            buttons:[
+              {
+                type:"web_url",
+                url:"https://petersfancybrownhats.com",
+                title:"View Website"
+              },{
+                type:"postback",
+                title:"Start Chatting",
+                payload:"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
           }
         ]
       }
